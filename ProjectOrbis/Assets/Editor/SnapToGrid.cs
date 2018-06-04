d2d257f2aa1a5fbbe53aca1b8659aa005931d343
yaboipapasmurf <39662979+yaboipapasmurf@users.noreply.github.com>
2018-06-04 10:15:35 +1000

    added a bunch of cool stuff
    e.g. Level Generator

---
+++ ProjectOrbis/Assets/Editor/SnapToGrid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Transform))]
+public class SnapToGrid : Editor {
+
+    
+
+    public override void OnInspectorGUI()
+    {
+        Transform trans = (Transform)target;
+
+        base.OnInspectorGUI();
+
+        float X = trans.position.x;
+        float Y = trans.position.y;
+
+        if (GUILayout.Button("snap")) {
+
+            trans.position = new Vector2(Mathf.Round(X), Mathf.Round(Y));
+        }
+
+    }
+
+
+}
---
+++ ProjectOrbis/Assets/Scripts/MapCreator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour {
+
+    public GameObject crate;
+    public Texture2D level;
+    public GameObject metalFloor;
+
+	// Use this for initialization
+	void Awake () {
+
+        int width = level.width;
+        int height = level.height;
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+
+                Color col = level.GetPixel(x, y);
+
+                if (col == Color.black) {
+                    Instantiate(crate, new Vector2(x, y), Quaternion.identity);
+
+
+                } else if (col == Color.white) {
+                    Instantiate(metalFloor, new Vector2(x, y), Quaternion.identity);
+                }
+
+            }
+              
+
+        }
+	}
+	
+	
+}

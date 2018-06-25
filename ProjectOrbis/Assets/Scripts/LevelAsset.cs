780c4c8c44b5664cbb3e40f46d531ebc7f5c6c47
yaboipapasmurf <39662979+yaboipapasmurf@users.noreply.github.com>
2018-06-25 16:43:16 +1000

    yeet

---
+++ ProjectOrbis/Assets/Scripts/LevelAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Level", menuName = "Level")]
+public class LevelAsset : ScriptableObject {
+    public string LevelName;
+    [Range(0, 3)]
+    public int Difficulty;
+    public Texture2D LevelTexture;
+
+
+
+
+}
+
+

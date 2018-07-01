fd81001abf07872591a61814af024c8d3bee6ba4
yaboipapasmurf <39662979+yaboipapasmurf@users.noreply.github.com>
2018-07-01 18:56:15 +1000

    zebra

---
+++ ProjectOrbis/Assets/Scripts/Plugins/SimpleSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class SimpleSerializer
+{
+
+    public static void SaveInt(string key, int value)
+    {
+        string PATH = Application.streamingAssetsPath + "/" + key + ".txt";
+        File.WriteAllText(PATH, value.ToString());
+    }
+
+    public static int LoadInt(string key)
+    {
+        string PATH = Application.streamingAssetsPath + "/" + key + ".txt";
+        string result = File.ReadAllText(PATH);
+        return int.Parse(result);
+    }
+
+
+}
